0bb43da86fde5650e8652f8d90c49f974270d4e2
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2021-02-24 17:08:22 -0500

    b: response accordingly

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const register = () => {};
 
 const login = asyncHandler(async (req, res) => {
   let { email, password } = req.body;
-  let userExists = await User.find({});
+  let userExists = await User.findOne({ email });
   res.send(`Message: ${userExists}.`);
   if (userExists) {
     res.json({ message: userExists.password });
